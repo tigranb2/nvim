efc8b9a575fdece2e83cdd0fdde1361c3a465568
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-01-01 11:57:46 -0500

    added toggleterm and updated cmp bindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,5 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open vim file explorer" })
+
+vim.keymap.set("t", "<S-Space>", "<Space>", {})
+vim.keymap.set("t", "<S-Enter>", "<Enter>", {})
+vim.keymap.set("t", "<S-BS>", "<BS>", {})
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -31,6 +31,11 @@ return {
         -- use opts = {} for passing setup options
         -- this is equivalent to setup({}) function
     },
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        config = true,
+    },
     -- lsp
     'neovim/nvim-lspconfig',
     "williamboman/mason.nvim",
@@ -38,7 +43,17 @@ return {
     "nvimtools/none-ls.nvim",
     -- autocompletion
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/nvim-cmp',
+    {
+        "hrsh7th/nvim-cmp",
+        opts = function(_, opts)
+            local cmp = require("cmp")
+            opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+                ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            })
+        end,
+    },
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
     'rafamadriz/friendly-snippets',
