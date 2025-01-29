a62adf811cee4058b1cf86d6fafcc6b1192da3f8
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-01-29 11:29:03 -0600

    scroll keybind updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,8 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open vim file explorer" })
-
 vim.keymap.set("t", "<S-Space>", "<Space>", {})
 vim.keymap.set("t", "<S-Enter>", "<Enter>", {})
 vim.keymap.set("t", "<S-BS>", "<BS>", {})
+
+vim.keymap.set("n", "<C-j>", "<C-e>", { desc = "Scroll down one line" })
+vim.keymap.set("n", "<C-k>", "<C-y>", { desc = "Scroll up one line" })
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open vim file explorer" })
